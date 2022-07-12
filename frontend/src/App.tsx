27e003b39640d0d6eec32424b6ae82a1e5a215bc
João Pedro <jpedro1580@gmail.com>
2022-07-11 23:25:17 -0300

    Structure App.tsx to be Header first, then main content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Header from './components/Header';
 import NotificationButton from './components/NotificationButton';
+import SalesCard from './components/SalesCard';
 
 function App () {
   return <>
-    <NotificationButton/>
-    <NotificationButton/>
-    <NotificationButton/>
+    <Header/>
+    <main>
+    <section id='sales'>
+      <div className='dsmeta-container'>
+        <SalesCard/>
+      </div>
+    </section>
+    </main>
   </>;
 }
 
