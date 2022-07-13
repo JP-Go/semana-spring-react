52dbac680510c35470557e34916846c2e6c7db45
João Pedro <jpedro1580@gmail.com>
2022-07-13 19:17:10 -0300

    Formatting

--- backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -1,20 +1,21 @@
 package com.devsuperior.dsmeta.entities;
 
 import java.time.LocalDate;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
 @Entity
 @Table(name = "tb_sales")
 public class Sale {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   private String sellerName;
   private Integer visited;
   private Integer deals;
