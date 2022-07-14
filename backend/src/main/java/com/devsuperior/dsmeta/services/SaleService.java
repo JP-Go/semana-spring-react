86ddbf61cb85db4b35d9859b24b3ddd9c12bd838
João Pedro <jpedro1580@gmail.com>
2022-07-13 21:53:47 -0300

    Implement Query Params 'maxDate' and 'minDate'

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,20 +1,28 @@
 package com.devsuperior.dsmeta.services;
 
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import com.devsuperior.dsmeta.entities.Sale;
-import com.devsuperior.dsmeta.repositories.SaleRepository;
-
 @Service
 public class SaleService {
 
-	@Autowired
-  private SaleRepository repository;
+  @Autowired private SaleRepository repository;
 
-  public Page<Sale> findSales(Pageable pageable) {
-    return repository.findAll(pageable);
+  public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
+    LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+    LocalDate aYearBeforeToday = today.minusYears(1);
+
+    LocalDate min = minDate.isEmpty() ? aYearBeforeToday : LocalDate.parse(minDate);
+    LocalDate max = maxDate.isEmpty() ? today : LocalDate.parse(maxDate);
+
+    return repository.findSales(min, max, pageable);
   }
 }
